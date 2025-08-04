1f7856724714ac43af3282ad2b2b93788a0d3f66
Thales Marega <thalesmarega@Thaless-MacBook-Pro.local>
2025-08-04 20:38:02 -0300

    init project

--- python-pix-agent/controller/main_controller.py
+++ python-pix-agent/controller/main_controller.py
@@ -2,6 +2,8 @@ from infra.openai_client import OpenAIClientFactory
 from langchain_openai.chat_models import ChatOpenAI
 from logger import get_logger
 from pipeline.openai import Key
+from model.graph_state import GraphState
+from langgraph.graph import StateGraph
 
 logger = get_logger(__name__)
 
@@ -14,7 +16,25 @@ class MainController:
         
         chat: ChatOpenAI = self.openai_client.create_basic_client()
 
-        print(chat.invoke("Olá, como você está?").content)
+    
+        # 5 - Criando o Graph
+        graph = StateGraph(GraphState)
+        # graph.add_node("responder", responder)
+        graph.set_entry_point("responder")
+        graph.set_finish_point("responder")
+
+        # 6 - Compilando o Grafo
+        export_graph = graph.compile()
+
+
+        
+    # def responder(state):
+    #     input_message = state.input
+    #     response = llm_model.invoke([HumanMessage(content=input_message)])
+    #     return GraphState(
+    #         input=state.input, 
+    #         output=response.content
+    #     )
 
 
 
