af7912497aba3154b041bc5afe99336b873b26a4
Thales Marega <thales.bm92@gmail.com>
2025-08-06 21:38:07 -0300

    atualizando update_limit

--- python-pix-agent/graph/nodes/limits/update_limit.py
+++ python-pix-agent/graph/nodes/limits/update_limit.py
@@ -1,4 +1,7 @@
 from graph.graph_state import GraphState
+from infra.openai_client import OpenAIClientFactory
+from pipeline.openai import Key
+from langchain_openai.chat_models import ChatOpenAI
 
 from datetime import datetime
 
@@ -8,9 +11,37 @@ logger = get_logger(__name__)
 def update_limit(state: GraphState) -> GraphState:
     logger.info("Node: Update Limit")
 
-    state.limit.value = 2500
-    state.limit.last_update = datetime.now()
+    api_key = Key.get_openai_key()
+    openai_client = OpenAIClientFactory(api_key=api_key)
+    chat: ChatOpenAI = openai_client.create_basic_client()
+
+    prompt = f"""
+        Você é um assistente de atendimento bancário.
+        
+        1. Se houver um valor monetário mencionado (por exemplo: "500", "R$ 2.000", "mil reais", "dois mil"), extraia apenas o valor em formato numérico, sem símbolos ou texto.
+            Retorne no formato JSON:
+            {{"tem_valor": true, "valor": 2000}}
+
+        2. Se não houver valor explícito, retorne uma resposta natural e breve pedindo ao cliente que informe o valor desejado para atualizar o limite.  
+            Retorne no formato JSON:
+            {{"tem_valor": false, "resposta": "Claro! Qual valor você quer definir como novo limite?"}}
+
+        Mensagem do cliente: "{state.user_message}"
+    """
+
+    response = chat.invoke(prompt)
+
+    if response.content["tem_valor"]:
+        state.limit.value = response.content["valor"]
+        state.limit.last_update = datetime.now()
+    else: 
+        None
 
     state.trace.append("update_limit")
+
+    logger.info("================================================")
+    logger.info(f"Prompt: {prompt}")
+    logger.info(f"Resposta: {response.content}")
+    logger.info("================================================")
     
     return state
