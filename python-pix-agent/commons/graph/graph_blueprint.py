d3b3dfda7ed3d2c1ad17da106775a45f497e26e6
Thales Marega <thales.bm92@gmail.com>
2025-08-09 19:54:15 -0300

    refatorado grafos

--- python-pix-agent/commons/graph/graph_blueprint.py
+++ python-pix-agent/commons/graph/graph_blueprint.py
@@ -17,6 +17,7 @@ class GraphBlueprintBuilder:
         """
         graph_builder = StateGraph(self.state_type)
         
+        self._add_entry_point(graph_builder, graph_blueprint)
         self._add_nodes(graph_builder, graph_blueprint)
         self._add_edges(graph_builder, graph_blueprint)
         self._add_endpoints(graph_builder, graph_blueprint)
@@ -24,22 +25,22 @@ class GraphBlueprintBuilder:
         
         return graph_builder.compile()
 
+    def _add_entry_point(self, graph_builder: StateGraph, graph_blueprint: GraphBlueprint):
+        """
+        Adiciona o ponto de entrada ao grafo.
+        """
+        graph_builder.set_entry_point(graph_blueprint.entry)
+
     def _add_nodes(self, graph_builder: StateGraph, graph_blueprint: GraphBlueprint):
         """
         Adiciona os nós ao grafo.
         """
         for n in graph_blueprint.nodes:
-            graph_builder.add_node(n.name, self._wrap(n.strategy_factory))
+            graph_builder.add_node(n.name, RunnableLambda(n.strategy_factory().build))
 
         if graph_blueprint.routers:
             for r in graph_blueprint.routers:
                 graph_builder.add_node(r.name, RunnableLambda(lambda st, f=r.func: {"next": f(st)}))
-
-    def _wrap(self, strategy_factory):
-        """
-        Padroniza o wrapping + decorators, retries, trace, etc.
-        """
-        return RunnableLambda(strategy_factory().build)
 
     def _add_edges(self, graph_builder: StateGraph, graph_blueprint: GraphBlueprint):
         """
@@ -47,8 +48,6 @@ class GraphBlueprintBuilder:
         """
         for e in graph_blueprint.edges:
             graph_builder.add_edge(e.src, e.dst)
-
-        graph_builder.set_entry_point(graph_blueprint.entry)
 
     def _add_endpoints(self, graph_builder: StateGraph, graph_blueprint: GraphBlueprint):
         """
@@ -59,11 +58,11 @@ class GraphBlueprintBuilder:
         for e in ends:
             graph_builder.add_edge(e, END)
 
-    def _after_build(self, graph_builder, graph_blueprint):
+    def _after_build(self, graph_builder: StateGraph, graph_blueprint: GraphBlueprint):
         """
         Imprime o grafo após a construção.
         """
-        threading.Thread(target=lambda: asyncio.run(self.print(graph_builder, graph_blueprint))).start()
+        threading.Thread(target=lambda: asyncio.run(self._print(graph_builder, graph_blueprint))).start()
 
-    async def print(self, graph_builder, graph_blueprint):
+    async def _print(self, graph_builder: StateGraph, graph_blueprint: GraphBlueprint):
         print_graph(graph_builder, graph_blueprint.id)
