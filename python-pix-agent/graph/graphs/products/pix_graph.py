c46f4059a8496d85f04559ac9074304d303640e7
Thales Marega <thales.bm92@gmail.com>
2025-08-18 22:00:59 -0300

    atualizado grafo de pix

--- python-pix-agent/graph/graphs/products/pix_graph.py
+++ python-pix-agent/graph/graphs/products/pix_graph.py
@@ -28,40 +28,40 @@ class PixGraph:
 
         graph_builder = StateGraph(GraphState)
 
-        graph_builder.add_node("verificar_chave_valor", RunnableLambda(CheckValueKeyNodeStrategy().build))
-        graph_builder.add_node("simular_pix", RunnableLambda(SimulatePixNodeStrategy().build))
-        graph_builder.add_node("efetivar_pix", RunnableLambda(EffectivePixNodeStrategy().build))
-        graph_builder.add_node("buscar_contato_pix", RunnableLambda(GetContactPixNodeStrategy().build))
-        graph_builder.add_node("verificar_data_pix", RunnableLambda(VerifyDatePixNodeStrategy().build))
-        graph_builder.add_node("consultar_limite", RunnableLambda(GetLimitNodeStrategy().build))
-        graph_builder.add_node("saldo", RunnableLambda(GetBalanceNodeStrategy().build))
-        graph_builder.add_node("comprovante", RunnableLambda(ReceiptNodeStrategy().build))
-        graph_builder.add_node("encerrar_fluxo_simples", RunnableLambda(FinishSimpleFlowNodeStrategy().build))
-        graph_builder.add_node("formatar_resposta", RunnableLambda(FormatAnswerFromStateNodeStrategy().build))
-        graph_builder.add_node("limpar_estado", RunnableLambda(CleanStateNodeStrategy().build))
+        graph_builder.add_node(CheckValueKeyNodeStrategy.name(), RunnableLambda(CheckValueKeyNodeStrategy().build))
+        graph_builder.add_node(SimulatePixNodeStrategy.name(), RunnableLambda(SimulatePixNodeStrategy().build))
+        graph_builder.add_node(EffectivePixNodeStrategy.name(), RunnableLambda(EffectivePixNodeStrategy().build))
+        graph_builder.add_node(GetContactPixNodeStrategy.name(), RunnableLambda(GetContactPixNodeStrategy().build))
+        graph_builder.add_node(VerifyDatePixNodeStrategy.name(), RunnableLambda(VerifyDatePixNodeStrategy().build))
+        graph_builder.add_node(GetLimitNodeStrategy.name(), RunnableLambda(GetLimitNodeStrategy().build))
+        graph_builder.add_node(GetBalanceNodeStrategy.name(), RunnableLambda(GetBalanceNodeStrategy().build))
+        graph_builder.add_node(ReceiptNodeStrategy.name(), RunnableLambda(ReceiptNodeStrategy().build))
+        graph_builder.add_node(FinishSimpleFlowNodeStrategy.name(), RunnableLambda(FinishSimpleFlowNodeStrategy().build))
+        graph_builder.add_node(FormatAnswerFromStateNodeStrategy.name(), RunnableLambda(FormatAnswerFromStateNodeStrategy().build))
+        graph_builder.add_node(CleanStateNodeStrategy.name(), RunnableLambda(CleanStateNodeStrategy().build))
 
-        graph_builder.set_entry_point("verificar_chave_valor")
+        graph_builder.set_entry_point(CheckValueKeyNodeStrategy.name())
 
         graph_builder.add_conditional_edges(
-            "verificar_chave_valor",
+            CheckValueKeyNodeStrategy.name(),
             self.decidir_proximo_no_depois_input,
             {
-                "saldo": "saldo",
-                "encerrar_fluxo_simples": "encerrar_fluxo_simples",
+                GetBalanceNodeStrategy.name(): GetBalanceNodeStrategy.name(),
+                FinishSimpleFlowNodeStrategy.name(): FinishSimpleFlowNodeStrategy.name(),
             }
         )
 
         # Pix
-        graph_builder.add_edge("verificar_data_pix", "saldo")
-        graph_builder.add_edge("saldo", "consultar_limite")
-        graph_builder.add_edge("consultar_limite", "buscar_contato_pix")
-        graph_builder.add_edge("buscar_contato_pix", "simular_pix")
-        graph_builder.add_edge("simular_pix", "efetivar_pix")
-        graph_builder.add_edge("efetivar_pix", "comprovante")
-        graph_builder.add_edge("comprovante", "formatar_resposta")
-        graph_builder.add_edge("formatar_resposta", "limpar_estado")
-        graph_builder.add_edge("limpar_estado", END)
-        graph_builder.add_edge("encerrar_fluxo_simples", END)
+        graph_builder.add_edge(VerifyDatePixNodeStrategy.name(), GetBalanceNodeStrategy.name())
+        graph_builder.add_edge(GetBalanceNodeStrategy.name(), GetLimitNodeStrategy.name())
+        graph_builder.add_edge(GetLimitNodeStrategy.name(), GetContactPixNodeStrategy.name())
+        graph_builder.add_edge(GetContactPixNodeStrategy.name(), SimulatePixNodeStrategy.name())
+        graph_builder.add_edge(SimulatePixNodeStrategy.name(), EffectivePixNodeStrategy.name())
+        graph_builder.add_edge(EffectivePixNodeStrategy.name(), ReceiptNodeStrategy.name())
+        graph_builder.add_edge(ReceiptNodeStrategy.name(), FormatAnswerFromStateNodeStrategy.name())
+        graph_builder.add_edge(FormatAnswerFromStateNodeStrategy.name(), CleanStateNodeStrategy.name())
+        graph_builder.add_edge(CleanStateNodeStrategy.name(), END)
+        graph_builder.add_edge(FinishSimpleFlowNodeStrategy.name(), END)
 
         graph = graph_builder.compile()
         logger.info("PixGraph criado")
@@ -76,6 +76,6 @@ class PixGraph:
         logger.info("================================================")
 
         if state.pix.has_value and state.pix.has_key:
-            return "saldo"
+            return GetBalanceNodeStrategy.name()
         else:
-            return "encerrar_fluxo_simples"
+            return FinishSimpleFlowNodeStrategy.name()
